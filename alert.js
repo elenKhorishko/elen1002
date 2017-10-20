ed297050730b19a8d47c13833e99d6a79bb1380b
Sashamokhno <sasha.mokhno@gmail.com>
2017-10-20 21:02:08 +0300

    lesson 5

---
+++ alert.js
@@ -0,0 +1,39 @@
+"use strict";
+
+var message;
+
+message = 'Hello'; // записали строку
+alert(message);
+
+
+var message = 'Hi'; // записали строку
+alert(message);
+
+var message = 'Hello';
+alert(message);
+
+message = 'World';
+alert(message);
+
+
+
+var message;
+
+message = 'Hello!';
+
+message = 'World!'; // заменили значение
+
+alert( message );
+
+
+var admin, name
+name = 'Василий';
+admin=name
+alert(admin);
+
+
+var first = 5;
+var second = 3;
+var result = first + second;
+alert( result);
+
