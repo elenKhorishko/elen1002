8fa17a158d739a8b3d6c972e9e29704855c42883
elena <elena.dnepr.2007@gmail.com>
2017-10-27 21:04:51 +0300

    commit lesson 7

---
+++ lesson7.js
@@ -0,0 +1,100 @@
+'use strict';
+/*
+// задача 1
+var arr = [],
+    x = '',
+    i;
+for (i = 0; i < 10; i++) {
+    x += 'x';
+    arr.push(x);
+}
+console.log(arr);
+
+
+// задача 3
+function arrayFill(s, count) {
+    var i,
+        res = '';
+    for (i = 0; i < count; i++) {
+        res += s;
+    }
+
+    return res;
+}
+console.log(arrayFill('123-', 10));
+
+
+// задача 2
+function arrayFill(s, count) {
+    var i,
+        res = '';
+    for (i = 0; i < count; i++) {
+        res += s;
+    }
+    return res;
+}
+
+var i,
+    arr = [];
+for (i = 1; i <= 9; i++) {
+    arr.push(arrayFill(i, i));
+}
+console.log(arr);
+
+
+// задача 4
+var arr = [3, 1, 2, 6, 9, 3, 6, 4, 5],
+    sum = 0;
+for (var j = 0; j < arr.length; j ++) {
+    sum += arr[j];
+    if (sum > 10) {
+        console.log(j+1); // alert(j+1)
+        break;
+    }
+}
+
+
+// задача 5
+var arr = [3, 1, 2, 6, 9, 3, 6, 4, 5],
+    arr2 = [];
+for (var j = arr.length - 1; j >= 0; j --) {
+    arr2.push(arr[j]);
+}
+alert(arr2);
+
+// задача 6
+var arr = [[1,2,3],[4,5],[6]],
+    res = 0;
+for (i = 0; i < arr.length; i++) {
+    for (j = 0; j < arr[i].length; j++) {
+        res += arr [i][j];
+    }
+}
+alert(res);
+
+// задача 7
+var arr = [[[1, 2], [3, 4]], [[5, 6], [7, 8]]],
+    res = 0;
+for (var i = 0; i < arr.length; i++) {
+    for (var j = 0; j < arr[i].length; j++) {
+        for (var k = 0; k < arr[j].length; k++) {
+            res += arr [i][j][k];
+        }
+    }
+}
+alert(res);
+
+*/
+
+
+//Получить последний элемент массива
+
+
+
+
+
+
+
+
+
+
