b4b5e59d5588db17928675f9a0e672d5bf5e747e
elena <elena.dnepr.2007@gmail.com>
2017-10-31 21:09:08 +0200

    commit HW lesson 8 Start

---
+++ HWlesson8.js
@@ -0,0 +1,18 @@
+'use strict';
+
+$(document).ready(function (){
+    // задача 1
+    $('h3').html('!!!');
+
+    // задача 2
+    $('h3').css('color','green');
+
+    //задача 3
+    $('h3').css({color: 'green', fontWeight: 'bold'});
+
+    //задача 4
+    $('.zzz').css('color', red);
+
+
+
+});
