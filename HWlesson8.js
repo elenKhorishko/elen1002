824f8fed68aaf96df62c27d20017b294d4af36f3
elenKhorishko <elena.dnepr.2007@gmail.com>
2017-11-01 21:18:10 +0200

    commit  HW lesson 8 - Stsrt

--- HWlesson8.js
+++ HWlesson8.js
@@ -2,17 +2,47 @@
 
 $(document).ready(function (){
     // задача 1
-    $('h3').html('!!!');
+    $('h2').html('!');
 
     // задача 2
-    $('h3').css('color','green');
+    $('h3').css('color','red');
 
     //задача 3
-    $('h3').css({color: 'green', fontWeight: 'bold'});
+    $('h2').css({'color': 'blue', 'font-size': '30px'});
 
     //задача 4
-    $('.zzz').css('color', red);
+    $('.test').css('color', 'red');
 
+    //задача 5
+    $('h2.test').css('color', 'orange');
+
+    //задача 6
+    $('p + p').css('color', 'green')
+
+    //задача 7 ??????????????????????????????????????????
+    $('li.www + li').css('color', 'yellow')
+
+
+
+
+
+
+
+    //задача 5
+ //   $(':header').css('color', 'red');
+
+    //задача 6
+//    $(':header.zzz').css('color', 'blue');
+
+    //задача 7
+//    $(':header:not(.zzz)').css('color', 'orange');
+
+    //задача 8
+  //  $('li').each(function (index, element) {
+ //       $(element).html(index+1);
+  //  })
+
+    //задача 9
 
 
 });
